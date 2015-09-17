6471d7e83909ea2c947a666dee676eabb906ad22
Salifu Mutaru <salifumutaru@gmail.com>
2015-09-17 21:53:41 +0000

    Working on login
    Not able to parse login detail to server

---
+++ resources/script.js
@@ -0,0 +1,29 @@
+function sendRequest(u){
+    // Send request to server
+    //u a url as a string
+    //async is type of request
+    var obj=$.ajax({url:u,async:false});
+    //Convert the JSON string to object
+    var result=$.parseJSON(obj.responseText);
+    return result;	//return object
+
+}
+
+$(function() {
+    $('#loginbtn').click(function(e) {
+      logIn($("#secretKey").val());
+    });
+});
+
+function logIn(secretKey){
+  var link = "http://10.10.33.191/mw/tanks.php?cmd=3&";
+  var strUrl = link+"secretKey="+secretKey;
+
+  var objResult = sendRequest(strUrl);
+  if(objResult.result == 1){
+    alert(objResult.message);
+    return;
+  }
+  alert(objResult.message);
+  return;
+}
